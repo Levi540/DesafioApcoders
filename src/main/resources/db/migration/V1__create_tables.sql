0dc18a19e295ec3928e1bcfa4f894ebc6e9238a2
Gabriel Levi Dalfovo <59920991+Levi540@users.noreply.github.com>
2022-01-19 20:48:03 -0300

    Initial commit

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `tenants` (
+  `id`        BIGINT      NOT NULL AUTO_INCREMENT,
+  `name`      VARCHAR(45) NOT NULL,
+  `age`       INT         NOT NULL,
+  `sex`       VARCHAR(1)  NOT NULL,
+  `telephone` VARCHAR(30) NOT NULL,
+  `email`     VARCHAR(70) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `units` (
+  `id`          BIGINT       NOT NULL AUTO_INCREMENT,
+  `owner`       VARCHAR(45)  NOT NULL,
+  `condominium` VARCHAR(45)  NOT NULL,
+  `address`     VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `unit_expenses` (
+  `id_unit_expenses`         BIGINT        NOT NULL AUTO_INCREMENT,
+  `unit_expense_description` VARCHAR(100)  NOT NULL,
+  `type_expense`             VARCHAR(45)   NOT NULL,
+  `amount`                   DECIMAL(10,2) NOT NULL,
+  `invoice_due_date`         DATE          NOT NULL,
+  `payment_status`           VARCHAR(20)   NOT NULL,
+  PRIMARY KEY (`id_unit_expenses`)
+);
